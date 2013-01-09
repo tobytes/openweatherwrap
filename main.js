1bab188e509743e18e1168f756e778a61a66767e
tobytes <mailtobyte@googlemail.com>
2013-01-09 23:43:49 +0100

    first two functions & module informations

--- main.js
+++ main.js
@@ -1,13 +1,81 @@
 var request = require('request');
-var url = 'http://openweathermap.org/data/2.1/weather/city/2935022?type=json';
-var responseJson = "";
 
+// base url for requests
+var baseUrl = 'http://www.openweathermap.org/data/';
+// OpenWeatherMap API version
+var apiVersion = '2.1';
 
-exports.getCity = function(callback) {
-    requestData(callback);
+// find
+
+/**
+ * Find weather stations near a point 
+ *
+ * @param {array} parameters
+ * @param {function} callback
+ * @return void
+ */
+exports.findStationsNearPoint = function (parameters, callback) {
+    var url = buildUrl('find/station/', parameters);
+    console.log(url);
+    queryData(url, callback);
 }
 
-function requestData(callback) {
+// weather
+
+/**
+ * Queries current weather in a city defined
+ * by the id
+ *
+ * @param {array} parameters
+ * @param {function} callback
+ * @return void
+ */
+exports.getCurrentWeatherByCityId = function (parameters, callback) {
+    var url = buildUrl('weather/city/', parameters);
+    console.log(url);
+    queryData(url, callback);
+}
+// forcast
+// weather station
+// history station
+
+
+/**
+ * Concatenates baseUrl, path and parameters to an url
+ *
+ * @param {string} path for the request
+ * @param {array} parameters for the request 
+ * @return {string} the concatenated url string
+ */
+function buildUrl(path, parameters) {
+    return baseUrl + apiVersion + '/' + path + '?' + encodeParameters(parameters);
+}
+
+/**
+ * Encodes and concatenates the given parameters  
+ *
+ * @param {array} parameters for the request
+ * @return {string} concatenated and encoded parameters 
+ */
+function encodeParameters(parameters) {
+    return Object.keys(parameters).map(function(key) {
+        if (!isNaN(parseInt(key))) {
+            return encodeURIComponent(parameters[key]) + '/';
+        } else {
+            return [key, parameters[key]].map(encodeURIComponent).join("=");
+        }
+    }).join("&");
+}
+
+/**
+ * Queries the given url and return
+ * the response to callback if successful
+ * 
+ * @param {string} url 
+ * @param {function} callback 
+ * @return void
+ */
+function queryData(url, callback) {
     request(url, function (error, response, body) {
         if(!error && response.statusCode == 200) {
             callback(JSON.parse(body));
